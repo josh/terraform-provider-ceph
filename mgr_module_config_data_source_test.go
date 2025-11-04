55bef43e72b288dd47ece1501fd320f2d02f97dc
Joshua Peek <josh@users.noreply.github.com>
2025-11-04 12:40:52 -0800

    Improve handling of mgr module config values

--- mgr_module_config_data_source_test.go
+++ mgr_module_config_data_source_test.go
@@ -1,11 +1,13 @@
 package main
 
 import (
+	"fmt"
 	"testing"
 
 	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
 	"github.com/hashicorp/terraform-plugin-testing/knownvalue"
 	"github.com/hashicorp/terraform-plugin-testing/statecheck"
+	"github.com/hashicorp/terraform-plugin-testing/terraform"
 	"github.com/hashicorp/terraform-plugin-testing/tfjsonpath"
 )
 
@@ -50,6 +52,66 @@ func TestAccCephMgrModuleConfigDataSource(t *testing.T) {
 						knownvalue.NotNull(),
 					),
 				},
+			},
+		},
+	})
+}
+
+func TestAccCephMgrModuleConfigDataSource_largeIntegerValues(t *testing.T) {
+	detachLogs := cephDaemonLogs.AttachTestFunction(t)
+	defer detachLogs()
+
+	resource.Test(t, resource.TestCase{
+		ProtoV6ProviderFactories: testAccProtoV6ProviderFactories,
+		Steps: []resource.TestStep{
+			{
+				PreConfig: func() {
+					err := setCephMgrModuleConfigValue("dashboard", "jwt_token_ttl", "31556952")
+					if err != nil {
+						t.Fatalf("Failed to set config value out of band: %v", err)
+					}
+				},
+				ConfigVariables: testAccProviderConfig(),
+				Config: testAccProviderConfigBlock + `
+					data "ceph_mgr_module_config" "test" {
+						module_name = "dashboard"
+					}
+				`,
+				ConfigStateChecks: []statecheck.StateCheck{
+					statecheck.ExpectKnownValue(
+						"data.ceph_mgr_module_config.test",
+						tfjsonpath.New("configs").AtMapKey("jwt_token_ttl"),
+						knownvalue.StringExact("31556952"),
+					),
+				},
+				Check: resource.ComposeAggregateTestCheckFunc(
+					resource.TestCheckResourceAttr("data.ceph_mgr_module_config.test", "configs.jwt_token_ttl", "31556952"),
+					func(s *terraform.State) error {
+						rs, ok := s.RootModule().Resources["data.ceph_mgr_module_config.test"]
+						if !ok {
+							return fmt.Errorf("data source not found")
+						}
+
+						ttl := rs.Primary.Attributes["configs.jwt_token_ttl"]
+						if ttl != "31556952" {
+							return fmt.Errorf("expected jwt_token_ttl='31556952', got '%s'", ttl)
+						}
+						return nil
+					},
+					func(s *terraform.State) error {
+						return assertCephMgrModuleConfigValue("dashboard", "jwt_token_ttl", "31556952")
+					},
+				),
+			},
+			{
+				PreConfig: func() {
+					err := removeCephMgrModuleConfigValue("dashboard", "jwt_token_ttl")
+					if err != nil {
+						t.Fatalf("Failed to remove config value out of band: %v", err)
+					}
+				},
+				ConfigVariables: testAccProviderConfig(),
+				Config:          testAccProviderConfigBlock,
 			},
 		},
 	})
