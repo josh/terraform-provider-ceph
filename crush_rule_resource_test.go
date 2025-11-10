293996d21744945e5f90259de9f2aa3b646dc926
Joshua Peek <josh@users.noreply.github.com>
2025-11-10 10:06:24 -0800

    Add crush rule resource

---
+++ crush_rule_resource_test.go
@@ -0,0 +1,282 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"os/exec"
+	"regexp"
+	"testing"
+	"time"
+
+	"github.com/hashicorp/terraform-plugin-testing/helper/acctest"
+	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
+	"github.com/hashicorp/terraform-plugin-testing/knownvalue"
+	"github.com/hashicorp/terraform-plugin-testing/statecheck"
+	"github.com/hashicorp/terraform-plugin-testing/terraform"
+	"github.com/hashicorp/terraform-plugin-testing/tfjsonpath"
+)
+
+func TestAccCephCrushRuleResource_replicated(t *testing.T) {
+	detachLogs := cephDaemonLogs.AttachTestFunction(t)
+	defer detachLogs()
+
+	ruleName := fmt.Sprintf("test-replicated-%s", acctest.RandStringFromCharSet(10, acctest.CharSetAlphaNum))
+
+	resource.Test(t, resource.TestCase{
+		ProtoV6ProviderFactories: testAccProtoV6ProviderFactories,
+		CheckDestroy:             testAccCheckCephCrushRuleDestroy,
+		Steps: []resource.TestStep{
+			{
+				ConfigVariables: testAccProviderConfig(),
+				Config: testAccProviderConfigBlock + fmt.Sprintf(`
+					resource "ceph_crush_rule" "test" {
+					  name           = %q
+					  pool_type      = "replicated"
+					  failure_domain = "host"
+					}
+				`, ruleName),
+				ConfigStateChecks: []statecheck.StateCheck{
+					statecheck.ExpectKnownValue(
+						"ceph_crush_rule.test",
+						tfjsonpath.New("name"),
+						knownvalue.StringExact(ruleName),
+					),
+					statecheck.ExpectKnownValue(
+						"ceph_crush_rule.test",
+						tfjsonpath.New("pool_type"),
+						knownvalue.StringExact("replicated"),
+					),
+					statecheck.ExpectKnownValue(
+						"ceph_crush_rule.test",
+						tfjsonpath.New("failure_domain"),
+						knownvalue.StringExact("host"),
+					),
+					statecheck.ExpectKnownValue(
+						"ceph_crush_rule.test",
+						tfjsonpath.New("rule_id"),
+						knownvalue.NotNull(),
+					),
+					statecheck.ExpectKnownValue(
+						"ceph_crush_rule.test",
+						tfjsonpath.New("type"),
+						knownvalue.Int64Exact(1),
+					),
+				},
+				Check: resource.ComposeAggregateTestCheckFunc(
+					checkCephCrushRuleExists(t, ruleName),
+					resource.TestCheckResourceAttr("ceph_crush_rule.test", "name", ruleName),
+					resource.TestCheckResourceAttr("ceph_crush_rule.test", "pool_type", "replicated"),
+					resource.TestCheckResourceAttr("ceph_crush_rule.test", "failure_domain", "host"),
+					resource.TestCheckResourceAttrSet("ceph_crush_rule.test", "rule_id"),
+					resource.TestCheckResourceAttr("ceph_crush_rule.test", "type", "1"),
+				),
+			},
+			{
+				ConfigVariables:                      testAccProviderConfig(),
+				ResourceName:                         "ceph_crush_rule.test",
+				ImportState:                          true,
+				ImportStateVerify:                    true,
+				ImportStateId:                        ruleName,
+				ImportStateVerifyIdentifierAttribute: "name",
+				ImportStateVerifyIgnore:              []string{"pool_type", "failure_domain", "device_class", "profile", "root"},
+			},
+		},
+	})
+}
+
+func TestAccCephCrushRuleResource_erasure(t *testing.T) {
+	detachLogs := cephDaemonLogs.AttachTestFunction(t)
+	defer detachLogs()
+
+	ruleName := fmt.Sprintf("test-erasure-%s", acctest.RandStringFromCharSet(10, acctest.CharSetAlphaNum))
+	profileName := fmt.Sprintf("test-profile-%s", acctest.RandStringFromCharSet(10, acctest.CharSetAlphaNum))
+
+	resource.Test(t, resource.TestCase{
+		ProtoV6ProviderFactories: testAccProtoV6ProviderFactories,
+		CheckDestroy:             testAccCheckCephCrushRuleDestroy,
+		Steps: []resource.TestStep{
+			{
+				ConfigVariables: testAccProviderConfig(),
+				Config: testAccProviderConfigBlock + fmt.Sprintf(`
+					resource "ceph_erasure_code_profile" "test" {
+					  name                 = %q
+					  k                    = 2
+					  m                    = 1
+					  crush_failure_domain = "osd"
+					}
+
+					resource "ceph_crush_rule" "test" {
+					  name           = %q
+					  pool_type      = "erasure"
+					  failure_domain = "osd"
+					  profile        = ceph_erasure_code_profile.test.name
+					}
+				`, profileName, ruleName),
+				ConfigStateChecks: []statecheck.StateCheck{
+					statecheck.ExpectKnownValue(
+						"ceph_crush_rule.test",
+						tfjsonpath.New("name"),
+						knownvalue.StringExact(ruleName),
+					),
+					statecheck.ExpectKnownValue(
+						"ceph_crush_rule.test",
+						tfjsonpath.New("pool_type"),
+						knownvalue.StringExact("erasure"),
+					),
+					statecheck.ExpectKnownValue(
+						"ceph_crush_rule.test",
+						tfjsonpath.New("failure_domain"),
+						knownvalue.StringExact("osd"),
+					),
+					statecheck.ExpectKnownValue(
+						"ceph_crush_rule.test",
+						tfjsonpath.New("profile"),
+						knownvalue.StringExact(profileName),
+					),
+					statecheck.ExpectKnownValue(
+						"ceph_crush_rule.test",
+						tfjsonpath.New("rule_id"),
+						knownvalue.NotNull(),
+					),
+					statecheck.ExpectKnownValue(
+						"ceph_crush_rule.test",
+						tfjsonpath.New("type"),
+						knownvalue.Int64Exact(3),
+					),
+				},
+				Check: resource.ComposeAggregateTestCheckFunc(
+					checkCephCrushRuleExists(t, ruleName),
+					resource.TestCheckResourceAttr("ceph_crush_rule.test", "name", ruleName),
+					resource.TestCheckResourceAttr("ceph_crush_rule.test", "pool_type", "erasure"),
+					resource.TestCheckResourceAttr("ceph_crush_rule.test", "type", "3"),
+				),
+			},
+			{
+				ConfigVariables:                      testAccProviderConfig(),
+				ResourceName:                         "ceph_crush_rule.test",
+				ImportState:                          true,
+				ImportStateVerify:                    true,
+				ImportStateId:                        ruleName,
+				ImportStateVerifyIdentifierAttribute: "name",
+				ImportStateVerifyIgnore:              []string{"pool_type", "failure_domain", "device_class", "profile", "root"},
+			},
+		},
+	})
+}
+
+func TestAccCephCrushRuleResource_withDeviceClass(t *testing.T) {
+	detachLogs := cephDaemonLogs.AttachTestFunction(t)
+	defer detachLogs()
+
+	ruleName := fmt.Sprintf("test-device-class-%s", acctest.RandStringFromCharSet(10, acctest.CharSetAlphaNum))
+
+	resource.Test(t, resource.TestCase{
+		ProtoV6ProviderFactories: testAccProtoV6ProviderFactories,
+		CheckDestroy:             testAccCheckCephCrushRuleDestroy,
+		Steps: []resource.TestStep{
+			{
+				ConfigVariables: testAccProviderConfig(),
+				Config: testAccProviderConfigBlock + fmt.Sprintf(`
+					resource "ceph_crush_rule" "test" {
+					  name           = %q
+					  pool_type      = "replicated"
+					  failure_domain = "osd"
+					  device_class   = "hdd"
+					}
+				`, ruleName),
+				ConfigStateChecks: []statecheck.StateCheck{
+					statecheck.ExpectKnownValue(
+						"ceph_crush_rule.test",
+						tfjsonpath.New("name"),
+						knownvalue.StringExact(ruleName),
+					),
+					statecheck.ExpectKnownValue(
+						"ceph_crush_rule.test",
+						tfjsonpath.New("device_class"),
+						knownvalue.StringExact("hdd"),
+					),
+				},
+				Check: resource.ComposeAggregateTestCheckFunc(
+					checkCephCrushRuleExists(t, ruleName),
+					resource.TestCheckResourceAttr("ceph_crush_rule.test", "device_class", "hdd"),
+				),
+			},
+		},
+	})
+}
+
+func TestAccCephCrushRuleResource_InvalidPoolType(t *testing.T) {
+	resource.Test(t, resource.TestCase{
+		ProtoV6ProviderFactories: testAccProtoV6ProviderFactories,
+		Steps: []resource.TestStep{
+			{
+				ConfigVariables: testAccProviderConfig(),
+				Config: testAccProviderConfigBlock + `
+					resource "ceph_crush_rule" "test" {
+					  name           = "test-invalid-type"
+					  pool_type      = "invalid"
+					  failure_domain = "host"
+					}
+				`,
+				ExpectError: regexp.MustCompile(`Attribute pool_type value must be one of`),
+			},
+		},
+	})
+}
+
+func checkCephCrushRuleExists(t *testing.T, ruleName string) resource.TestCheckFunc {
+	return func(s *terraform.State) error {
+		ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+		defer cancel()
+
+		cmd := exec.CommandContext(ctx, "ceph", "--conf", testConfPath, "osd", "crush", "rule", "dump", ruleName, "-f", "json")
+		output, err := cmd.Output()
+		if err != nil {
+			return fmt.Errorf("failed to get CRUSH rule '%s': %w", ruleName, err)
+		}
+
+		var rule map[string]interface{}
+		if err := json.Unmarshal(output, &rule); err != nil {
+			return fmt.Errorf("failed to parse CRUSH rule output: %w", err)
+		}
+
+		if rule["rule_name"] != ruleName {
+			return fmt.Errorf("CRUSH rule name mismatch: expected %q, got %q", ruleName, rule["rule_name"])
+		}
+
+		return nil
+	}
+}
+
+func testAccCheckCephCrushRuleDestroy(s *terraform.State) error {
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	for _, rs := range s.RootModule().Resources {
+		if rs.Type != "ceph_crush_rule" {
+			continue
+		}
+
+		ruleName := rs.Primary.Attributes["name"]
+
+		cmd := exec.CommandContext(ctx, "ceph", "--conf", testConfPath, "osd", "crush", "rule", "ls", "-f", "json")
+		output, err := cmd.Output()
+		if err != nil {
+			return fmt.Errorf("failed to list CRUSH rules: %w", err)
+		}
+
+		var rules []string
+		if err := json.Unmarshal(output, &rules); err != nil {
+			return fmt.Errorf("failed to parse CRUSH rule list: %w", err)
+		}
+
+		for _, rule := range rules {
+			if rule == ruleName {
+				return fmt.Errorf("CRUSH rule %q still exists in Ceph", ruleName)
+			}
+		}
+	}
+
+	return nil
+}
--- provider.go
+++ provider.go
@@ -165,6 +165,7 @@ func (p *CephProvider) Resources(ctx context.Context) []func() resource.Resource
 	return []func() resource.Resource{
 		newAuthResource,
 		newConfigResource,
+		newCrushRuleResource,
 		newErasureCodeProfileResource,
 		newMgrModuleConfigResource,
 		newRGWS3KeyResource,
